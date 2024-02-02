51f0860f434e2bbb5835074e3f9a6c06ce999fe9
Frax22 <80818620+Frax22@users.noreply.github.com>
2024-02-02 11:25:09 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
     let menuBtn = document.querySelector('.toggle')
     let navList = document.querySelector('.nav__list')
+    let closeMenuBtn = document.querySelector('.nav__close')
 
     menuBtn.addEventListener('click', ()=> {
-        navList.classList.toggle('active')
+        navList.classList.toggle('show__menu')
         menuBtn.classList.toggle('change')
-        
+        closeMenuBtn.classList.toggle('show')        
+    })
+
+    closeMenuBtn.addEventListener('click', ()=> {
+        navList.classList.toggle('show__menu')
+        menuBtn.classList.toggle('change')
+        closeMenuBtn.classList.remove('show')        
     })
 
     window.addEventListener('scroll', ()=> {

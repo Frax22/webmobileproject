b385ee853cb2dd001139050d23ed3ec402be61ab
Frax22 <80818620+Frax22@users.noreply.github.com>
2024-02-02 12:08:07 +0100

    Add files via upload

---
+++ js/js/app.js
@@ -0,0 +1,34 @@
+    // =======HAMBURGER MENU=======
+    
+    let menuBtn = document.querySelector('.toggle')
+    let navList = document.querySelector('.nav__list')
+    let closeMenuBtn = document.querySelector('.nav__close')
+
+    menuBtn.addEventListener('click', ()=> {
+        navList.classList.toggle('show__menu')
+        menuBtn.classList.toggle('change')
+        closeMenuBtn.classList.toggle('show')        
+    })
+
+    closeMenuBtn.addEventListener('click', ()=> {
+        navList.classList.toggle('show__menu')
+        menuBtn.classList.toggle('change')
+        closeMenuBtn.classList.remove('show')        
+    })
+
+
+    // =======SCROLL TO TOP BUTTON=======
+    
+    window.addEventListener('scroll', ()=> {
+        if(window.scrollY > 200 && window.innerWidth > 768) {
+            scrollBtn.style.display = "flex"
+        } else {
+            scrollBtn.style.display = "none"
+        }        
+    })
+
+    let scrollBtn = document.querySelector('.scroll__top')
+
+    scrollBtn.addEventListener('click', ()=> {
+        window.scrollTo(0,0)
+    })
